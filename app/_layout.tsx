75f1c65e3e1227b47a64caebc32350981dcc5006
Leonardo Ferreira Monteiro da Silva <leonardo@tefra.com.br>
2025-11-11 10:49:43 -0300

    Add theme toggle

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,20 @@
 import { Stack } from 'expo-router';
 import React from 'react';
-import { Provider as PaperProvider } from 'react-native-paper';
+import { IconButton, Provider as PaperProvider } from 'react-native-paper';
 import { FavoritesProvider } from '../src/contexts/FavoritesContext';
 import { ThemeProvider, useThemeContext } from '../src/contexts/ThemeContext';
 
 function InnerLayout() {
-  const { theme } = useThemeContext();
+  const { theme, toggleTheme} = useThemeContext();
 
   return (
     <PaperProvider theme={theme}>
       <FavoritesProvider>
         <Stack>
-          <Stack.Screen name="index" options={{ title: 'Lista de Itens' }} />
+          <Stack.Screen name="index" options={{
+            title: 'Lista de Itens',
+            headerRight: () => <IconButton icon="theme-light-dark" onPress={toggleTheme} />
+            }} />
           <Stack.Screen name="favorites" options={{ title: 'Favoritos' }} />
         </Stack>
       </FavoritesProvider>
